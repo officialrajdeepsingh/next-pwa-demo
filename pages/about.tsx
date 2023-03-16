19f7f62b394d8a8934929cf4cf921a8926193b66
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-16 16:45:39 +0530

    create about page

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+    return (
+        <>
+            <h1> About page </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium modi consectetur incidunt nulla, quos libero dignissimos fugiat quas numquam velit quasi, magni nemo atque quae. Nam autem enim ipsum.
+            </p>
+
+        </>
+    )
+
+}
