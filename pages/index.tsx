6ce805ea6f634225e961f8caeac3e7f870971695
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-16 19:45:37 +0530

    Remove the unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Header from "@/components/Header";
-import Footer from "@/components/Footer"
-import Card from "@/components/Card"
-
-const inter = Inter({ subsets: ['latin'] })
-
+import Card from "@/components/Card";
 export default function Home() {
   return (
     <>
-
-      <Card />
-      <Card />
-      <Card />
-
       <Card />
       <Card />
       <Card />
       <Card />
       <Card />
-
+      <Card />
+      <Card />
+      <Card />
     </>
   )
 }
