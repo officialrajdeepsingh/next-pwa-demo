8032652e20231241884324f9ab835589a147b7f8
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-16 21:21:22 +0530

    fix layout responsive

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ function Layout({ children }) {
 
   return <>
     <Header />
-    <main className="container my-12 mx-auto grid grid-cols-4 gap-5">
+    <main className="container my-12 mx-auto grid grid-cols-1 gap-2 md:gap-3 lg:gap-4  lg:grid-cols-3  md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4">
     {children}
     </main>
     <Footer />
