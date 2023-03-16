4a3e839e5f9b1a00f08fd1223bbdc9a1229c2c08
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-16 16:45:53 +0530

    create contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,14 @@
+export default function contact() {
+
+  return (
+  <>
+  
+  <h1> Contact page </h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium modi consectetur incidunt nulla, quos libero dignissimos fugiat quas numquam velit quasi, magni nemo atque quae. Nam autem enim ipsum.
+  </p>
+
+  </>
+  )
+
+}
