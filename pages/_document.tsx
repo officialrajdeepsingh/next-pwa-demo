4fe8177874681b296dbbedb61dd90195fb8a96eb
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-17 16:36:05 +0530

    set msaaplication config none

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,20 +11,21 @@ export default function Document() {
         <meta name="description" content="Best PWA App in the world" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-config" content="none" /> {/* learn more  https://blog.giantgeek.com/?p=1418 */}
         <meta name="msapplication-TileColor" content="#2B5797" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
 
         <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
         <link rel="apple-touch-icon" sizes="150x150" href="/icons/150x150.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
-        <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+
+        <link rel="apple-touch-icon" sizes="284x284" href="/icons/284x284.png" />
+        <link rel="apple-touch-icon" sizes="310x310" href="/icons/310x310.png" />
 
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/30x30.png" />
+
         <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
         <link rel="shortcut icon" href="/favicon.ico" />
 
         <meta property="og:type" content="website" />
